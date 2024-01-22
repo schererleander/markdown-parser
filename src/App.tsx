1d977707c474cdfc6d4ffdb86dbe16b2cae7aa89
schererleander <leander@leander-scherer.de>
2024-01-22 22:46:36 +0100

    feat: create parseMardown function

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
 import React, { useState } from 'react';
-import { parseHeading } from './heading';
+import { headingParse } from './utilities/heading';
+import { blockquoteParse } from './utilities/blockquote';
+import { horizontalLineParse } from './utilities/horizonLine';
+import { boldParse } from './utilities/bold';
+import { underlineParse } from './utilities/underline';
+import { italicParse } from './utilities/italics';
+import { imageParse } from './utilities/image';
+import { highlightParse } from './utilities/highlight';
+import { codeParse } from './utilities/code';
 
 function App() {
   const [markdownInput, setMarkdownInput] = useState("")
   const parseMarkdown = (markdownText: string) => {
     const lines = markdownText.split("\n")
     const renderLines = lines.map((line, index) => {
-      const heading = parseHeading(line)
+      
+      const heading = headingParse(line)
       if(heading) {
         return heading
       }
-      return <p key={index}>{line}</p>
+
+      const blockquote = blockquoteParse(line)
+      if(blockquote) {
+        return blockquote
+      }
+
+      const horizontalLine = horizontalLineParse(line)
+      if(horizontalLine) {
+        return horizontalLine
+      }
+
+      const image = imageParse(line)
+      if(image) {
+        return image
+      }
+      
+      let text = line
+      text = boldParse(text)
+      text = underlineParse(text)
+      text = italicParse(text)
+      text = highlightParse(text)
+      text = codeParse(text)
+
+      return <p key={index} dangerouslySetInnerHTML={{__html: text}} />
     })
     return renderLines
   }
@@ -30,7 +62,6 @@ function App() {
         </div>
         <div className='preview'>
           {parseMarkdown(markdownInput)}
-          <hr></hr>
         </div>
       </main>
     </>
