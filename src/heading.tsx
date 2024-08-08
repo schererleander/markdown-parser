7eaea93d5601a52db4ce669d226d785327d4f9dd
schererleander <leander@leander-scherer.de>
2024-08-08 13:56:27 +0200

    chore: remove duplicate

--- src/heading.tsx
+++
@@ -1,9 +0,0 @@
-export const parseHeading = (line: string) => {
-    if(line.startsWith("###### ")) return <h6>{line.slice(6)}</h6>
-    if(line.startsWith("##### ")) return <h5>{line.slice(5)}</h5>
-    if(line.startsWith("#### ")) return <h4>{line.slice(4)}</h4>
-    if(line.startsWith("### ")) return <h3>{line.slice(3)}</h3>
-    if(line.startsWith("## ")) return <h2>{line.slice(2)}</h2>
-    if(line.startsWith("# ")) return <h1>{line.slice(1)}</h1>
-    return null
-}
